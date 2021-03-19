9217b0acf7a895d3b5a2493d7db7ec34db20c1a6
Zofux <70540447+Zofux@users.noreply.github.com>
2021-03-19 19:58:48 +0100

    Whitelist Update

--- client/src/pages/AppListPage/index.jsx
+++ client/src/pages/AppListPage/index.jsx
@@ -14,7 +14,7 @@ export function AppListPage({
         getUserDetails()
             .then(({ data }) => {
                 if (data.whitelisted === false) {
-                    //return window.location.href = "/"
+                    return window.location.href = "/"
                 }
 
                 setLoading(false)
